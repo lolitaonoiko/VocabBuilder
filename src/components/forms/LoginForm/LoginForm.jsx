90f5645d41463cf87b0df655f7d0d8b5a4515d68
Lolita <lolita454462@gmail.com>
2025-07-28 21:24:26 +0300

    login form css

--- src/components/forms/LoginForm/LoginForm.jsx
+++ src/components/forms/LoginForm/LoginForm.jsx
@@ -5,6 +5,7 @@ import { loginValidationSchema } from '../../../validation/loginSchema';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import clsx from 'clsx';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
 
 const LoginForm = () => {
     const navigate = useNavigate();
@@ -22,24 +23,32 @@ const LoginForm = () => {
     } = useForm({ resolver: yupResolver(loginValidationSchema) });
 
     return (
-        <div>
-            <div>
-                <h2>Login</h2>
-                <p>Please enter your login details to continue using our service:</p>
-                <form>
-                    <div className={s.inptBox}>
-                        <input {...register('email')} className={clsx(s.input, errors.email?.message && s.inptError)} placeholder="Email" type="email" />
-                        {errors.email?.message && <ValidationInfo name={'warning'} errors={errors} type={'email'} />}
-                    </div>
-                    <div className={s.inptBox}>
-                        <input {...register('password')} className={clsx(s.input, errors.password?.message && s.inptError)} placeholder="Password" type={hiddenPass ? 'password' : 'text'} />
-                        {errors.password?.message && <ValidationInfo name={'warning'} errors={errors} type={'password'} />}
-                        <button className={s.iconBtn} onClick={handleOnClickEye}>
-                            {hiddenPass ? <FiEyeOff size={'20'} /> : <FiEye size={'20'} />}
-                        </button>
-                    </div>
-                </form>
+        <div className={s.loginBox}>
+            <div className={s.textBox}>
+                <h2 className={s.title}>Login</h2>
+                <p className={s.text}>Please enter your login details to continue using our service:</p>
             </div>
+
+            <form className={s.form} onSubmit={handleSubmit(data => console.log(data))}>
+                <div className={s.inptBox}>
+                    <input {...register('email')} className={clsx(s.input, errors.email?.message && s.inptError)} placeholder="Email" type="email" />
+                    {errors.email?.message && <ValidationInfo name={'warning'} errors={errors} type={'email'} />}
+                </div>
+                <div className={s.inptBox}>
+                    <input {...register('password')} className={clsx(s.input, errors.password?.message && s.inptError)} placeholder="Password" type={hiddenPass ? 'password' : 'text'} />
+                    {errors.password?.message && <ValidationInfo name={'warning'} errors={errors} type={'password'} />}
+                    <button className={s.iconBtn} onClick={handleOnClickEye}>
+                        {hiddenPass ? <FiEyeOff size={'20'} /> : <FiEye size={'20'} />}
+                    </button>
+                </div>
+                <button className={s.filledBtn} type="submit">
+                    Login
+                </button>
+            </form>
+
+            <button className={s.underlinedBtn} type="button" onClick={handleOnClickRedirect}>
+                Register
+            </button>
         </div>
     );
 };
