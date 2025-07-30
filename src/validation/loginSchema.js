1cb37f7c8ae285bf83654e91c63524c96110a6e4
Lolita <lolita454462@gmail.com>
2025-07-31 00:32:35 +0300

    login/register schemas

--- src/validation/loginSchema.js
+++ src/validation/loginSchema.js
@@ -3,14 +3,10 @@ import * as yup from 'yup';
 export const loginValidationSchema = yup.object({
     email: yup
         .string()
-        .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/, 'Email must be in the format: example@domain.com')
-        .min(3, 'Must be at least 3 characters long')
-        .max(50, 'Too long')
+        .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, 'Email must be in the format: example@domain.com')
         .required('Email is required'),
     password: yup
         .string()
-        .matches(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]+$/, 'Password must contain at least one uppercase letter and one number')
-        .required('Password is required')
-        .min(8, 'Must be at least 8 characters long')
-        .max(12, 'Too long'),
+        .matches(/^(?=.*[a-zA-Z]{6})(?=.*\d)[a-zA-Z\d]{7}$/, 'Password must consist of 6 English letters and 1 number')
+        .required('Password is required'),
 });
