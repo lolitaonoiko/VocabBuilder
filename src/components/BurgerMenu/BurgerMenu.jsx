63c135eec9c99ec69bda8d549dda2ea1db41c3df
Lolita <lolita454462@gmail.com>
2025-08-13 20:01:18 +0300

    check

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,6 +1,7 @@
 import { slide as Menu } from 'react-burger-menu';
 import { NavLink } from 'react-router-dom';
 const BurgerMenu = () => {
+    const burger = true;
     return (
         <div>
             <Menu>
