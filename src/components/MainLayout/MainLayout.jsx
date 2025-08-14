d516cf5c987731e90aaeb09026dc4053db525253
Lolita <lolita454462@gmail.com>
2025-08-14 18:49:02 +0300

    main layout check

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,10 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import s from './MainLayout.module.css';
 import { lazy } from 'react';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
+import { useSelector } from 'react-redux';
 
 const Header = lazy(() => import('../Header/Header'));
 
 const MainLayout = () => {
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+    const location = useLocation();
+    if (location.pathname === '/') {
+        return <Navigate to={isLoggedIn ? '/dictionary' : '/login'} replace />;
+    }
     return (
         <>
             <div className={s.container}>
