3ee026807313c4d5e890cd02fbda7191941794d4
Lolita <lolita454462@gmail.com>
2025-08-15 23:24:25 +0300

    add usernav component

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,13 +1,13 @@
+import { lazy } from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import { NavLink } from 'react-router-dom';
+
+const UserNav = lazy(() => import('../UserNav/UserNav'));
+
 const BurgerMenu = () => {
     return (
         <div>
-            <Menu>
-                <NavLink to="/dictionary">Dictionary</NavLink>
-                <NavLink to="/recommend">Recommend</NavLink>
-                <NavLink to="/training">Training</NavLink>
-            </Menu>
+            <UserNav />
         </div>
     );
 };
--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -1,5 +1,14 @@
+import { slide as Menu } from 'react-burger-menu';
+import { NavLink } from 'react-router-dom';
+
 const UserNav = () => {
-    return <>userNav</>;
+    return (
+        <Menu>
+            <NavLink to="/dictionary">Dictionary</NavLink>
+            <NavLink to="/recommend">Recommend</NavLink>
+            <NavLink to="/training">Training</NavLink>
+        </Menu>
+    );
 };
 
 export default UserNav;
